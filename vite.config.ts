75a9d1b3efee77dada972e37c999909daa465057
Avinash Shrivastav <avinash.shrivastav@revv.co.in>
2025-07-27 04:03:02 +0530

    pwa Added

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,41 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// import path from "path";
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    // alias: {
-    //   '@': path.resolve(__dirname, './src'),  // '@' ko 'src' folder ke liye map karo
-    // },
-  },
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate', // keeps service worker updated
+      includeAssets: ['favicon.svg', 'logo.png', 'robots.txt'], // static assets
+      manifest: {
+        name: 'My Shayari App',
+        short_name: 'Shayari',
+        description: 'Upload and Explore Shayari!',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
 })
